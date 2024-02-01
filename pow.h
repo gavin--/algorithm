5d7bf40d647bfd84b9842f8fda0b6eb33d521a73
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-01-31 16:45:33 -0800

    Update pow.h

--- pow.h
+++ pow.h
@@ -2,24 +2,24 @@ template<class T>
 T pow(T base, T exp) {
   T result = 1;
   while (exp > 0) {
-    if (base % 2 == 1) {
+    if (exp % 2 == 1) {
       result = result * base;
     }
     base = base * base;
     exp /= 2;
   }
-  return T;
+  return result;
 }
 
 template<class T>
 T pow(T base, T exp, T mod) {
   T result = 1;
   while (exp > 0) {
-    if (base % 2 == 1) {
+    if (exp % 2 == 1) {
       result = result * base % mod;
     }
     base = base * base % mod;
     exp /= 2;
   }
-  return T;
+  return result;
 }
