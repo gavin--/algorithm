2504f67b8755f0fcbb89fd4d40bec4597565004d
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-02-04 17:16:43 -0800

    Update sieve.h

--- sieve.h
+++ sieve.h
@@ -1,9 +1,9 @@
 #include <vector>
 
 template<class T>
-vector<T> sieve(T size) {
-  vector<T> primes;
-  vector<bool> composite(size);
+std::vector<T> sieve(T size) {
+  std::vector<T> primes;
+  std::vector<bool> composite(size);
   for (T i = 2; i < size; ++i) {
     if (!composite[i]) {
       primes.push_back(i);
