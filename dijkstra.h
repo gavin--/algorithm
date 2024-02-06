19db1cb82174b05437e912351acb4a3ffad0a43a
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-02-05 17:49:30 -0800

    Update dijkstra.h

--- dijkstra.h
+++ dijkstra.h
@@ -23,6 +23,26 @@ std::vector<U> dijkstra(std::span<Container<P<T, U>>> graph, T source) {
   return distance;
 }
 
+template<template<class, class> class Map, template <class...> class Container, template<class, class> class P, class T, class U>
+std::vector<U> dijkstra(const Map<T, Container<P<T, U>>>& graph, T source) {
+  std::vector<U> distance(graph.size(), std::numeric_limits<U>::max());
+  distance[source] = 0;
+  std::set<std::pair<U, T>> vertices;
+  vertices.emplace(0, source);
+  do {
+    auto vertex = vertices.begin()->second;
+    vertices.erase(vertices.begin());
+    for (auto [next, weight] : graph.at(vertex)) {
+      if (distance[next] > distance[vertex] + weight) {
+        vertices.erase({distance[next], next});
+        distance[next] = distance[vertex] + weight;
+        vertices.emplace(distance[next], next);
+      }
+    }
+  } while (!vertices.empty());
+  return distance;
+}
+
 template<std::ranges::contiguous_range R, class T>
 auto dijkstra(R&& r, T source) {
   return dijkstra(std::span<std::ranges::range_value_t<R>>(r), source);
