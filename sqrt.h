7584e53271348ae7c9c2a4f33b9f0e3930c5f604
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-02-28 15:54:12 -0800

    Update sqrt.h

--- sqrt.h
+++ sqrt.h
@@ -1,9 +1,6 @@
 template<class T>
 T sqrt(T n) {
-  if (n <= 1) {
-    return n;
-  }
-  auto result = n / 2, next = (result + n / result) / 2;
+  auto result = n, next = (result + n / result) / 2;
   while (result < next) {
     result = next;
     next = (result + n / result) / 2;
