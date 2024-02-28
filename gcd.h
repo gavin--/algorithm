1a1f1e103a78cfd98778ab12df75ec43d4e0cd33
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-02-27 19:38:58 -0800

    Update gcd.h

--- gcd.h
+++ gcd.h
@@ -1,3 +1,5 @@
+#include <tuple>
+
 template<class T>
 T gcd(T a, T b) {
   while (b != 0) {
@@ -6,4 +8,18 @@ T gcd(T a, T b) {
     a = t;
   }
   return a;
+}
+
+template<class T>
+T inverse(T a, T b) {
+  auto t = 0, next_t = 1, r = b, next_r = a;
+  while (next_r != 0) {
+    auto q = r / next_r;
+    std::tie(t, next_t) = std::make_tuple(next_t, t - q * next_t);
+    std::tie(r, next_r) = std::make_tuple(next_r, r - q * next_r);
+  }
+  if (t < 0) {
+    t += b;
+  }
+  return t;
 }
