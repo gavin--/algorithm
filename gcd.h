e699b85d2d5d575586e76ab5571d192e533ddca2
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-02-27 19:55:40 -0800

    Update gcd.h

--- gcd.h
+++ gcd.h
@@ -11,15 +11,21 @@ T gcd(T a, T b) {
 }
 
 template<class T>
-T inverse(T a, T b) {
+std::pair<T, T> extended_gcd(T a, T b) {
   auto t = 0, next_t = 1, r = b, next_r = a;
   while (next_r != 0) {
     auto q = r / next_r;
     std::tie(t, next_t) = std::make_tuple(next_t, t - q * next_t);
     std::tie(r, next_r) = std::make_tuple(next_r, r - q * next_r);
   }
-  if (t < 0) {
-    t += b;
+  return {r, t};
+}
+
+template<class T>
+T inverse(T a, T b) {
+  auto result = extended_gcd(a, b).second;
+  if (result < 0) {
+    result += b;
   }
-  return t;
+  return result;
 }
