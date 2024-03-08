81f0310d0e6a92dca4cdca28e3affdf304636060
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-03-07 20:09:03 -0800

    Update sqrt.h

--- sqrt.h
+++ sqrt.h
@@ -1,9 +1,11 @@
+#include <type_traits>
+
 template<class T>
 T sqrt(T n) {
   if (n <= 1) {
     return n;
   }
-  auto result = n, next = (result + n / result) / 2;
+  std::make_unsigned_t<T> result = n, next = (result + n / result) / 2;
   while (next < result) {
     result = next;
     next = (result + n / result) / 2;
