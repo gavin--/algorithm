d139b412537bb3069b0cba358f404ba40d7e1e28
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-03-08 17:51:56 -0800

    Update sqrt.h

--- sqrt.h
+++ sqrt.h
@@ -11,7 +11,7 @@ T sqrt(T n) {
   #if __cplusplus >= 202002L
     std::make_unsigned_t<T> result = T(1) << ((std::bit_width(std::make_unsigned_t<T>(n)) - 1) / 2 + 1);
   #else
-    std::make_unsigned_t<T> result = n;
+    std::make_unsigned_t<T> result = n / 2;
   #endif
   std::make_unsigned_t<T> next = (result + n / result) / 2;
   while (next < result) {
