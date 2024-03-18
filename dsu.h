d0ff2faef6497b65cd579f2df1cebe0d1232dfe1
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-03-17 18:35:57 -0700

    Update dsu.h

--- dsu.h
+++ dsu.h
@@ -2,39 +2,38 @@
 
 template <class T>
 class Dsu {
-  private:
-    std::vector<T> parent_, size_;
-
-  public:
-    Dsu(T size) : size_(size, 1), parent_([&]() {
-      std::vector<T> parent;
-      parent.reserve(size);
-      for (T i = 0; i < size; ++i) {
-        parent.emplace_back(i);
-      }
-      return parent;
-    }()) {}
-
-    T Find(T vertex) {
-      while (parent_[vertex] != vertex) {
-        parent_[vertex] = parent_[parent_[vertex]];
-        vertex = parent_[vertex];
-      }
-      return vertex;
+ private:
+  std::vector<T> parent_, size_;
+ public:
+  Dsu(T size) : size_(size, 1), parent_([&]() {
+    std::vector<T> parent;
+    parent.reserve(size);
+    for (T i = 0; i < size; ++i) {
+      parent.emplace_back(i);
     }
+    return parent;
+  }()) {}
 
-    bool Union(T left, T right) {
-      left = Find(left);
-      right= Find(right);
-      if (left == right) {
-        return false;
-      }
-      parent_[right] = left;
-      size_[left] += size_[right];
-      return true;
+  T Find(T vertex) {
+    while (parent_[vertex] != vertex) {
+      parent_[vertex] = parent_[parent_[vertex]];
+      vertex = parent_[vertex];
     }
+    return vertex;
+  }
 
-    T Size(T vertex) {
-      return size_[Find(vertex)];
+  bool Union(T left, T right) {
+    left = Find(left);
+    right= Find(right);
+    if (left == right) {
+      return false;
     }
+    parent_[right] = left;
+    size_[left] += size_[right];
+    return true;
+  }
+
+  T Size(T vertex) {
+    return size_[Find(vertex)];
+  }
 };
