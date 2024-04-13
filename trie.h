ad9117b583976f7a1f112c5752f69270a8971caa
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-04-13 15:17:13 -0700

    Update trie.h

--- trie.h
+++ trie.h
@@ -7,9 +7,7 @@ class PrefixTrie {
  private:
   std::vector<std::vector<T>> trie_;
  public:
-  PrefixTrie() {
-    trie_.emplace_back(std::numeric_limits<T>::max());
-  }
+  PrefixTrie() : trie_(1, std::vector<T>(std::numeric_limits<T>::max())) {}
   
   void Insert(const std::basic_string<T>& key) {
     T t = 0;
@@ -40,6 +38,53 @@ class PrefixTrie {
         return false;
       }
       t = trie_[t][key[i]];
+    }
+    return true;
+  }
+};
+
+template <class T>
+class Trie {
+ private:
+  class Node {
+    public:
+      std::vector<Node*> children_;
+      Node() : children_(std::numeric_limits<T>::max()) {}
+  };
+  Node* const root_ = new Node();
+ public:
+  void Insert(const std::basic_string<T>& key) {
+    Node* node = root_;
+    for (std::size_t i = 0; i <= key.size(); ++i) {
+      if (node->children_[key[i]] == nullptr) {
+        node->children_[key[i]] = new Node();
+      }
+      node = node->children_[key[i]];
+    }
+  }
+  
+  void Delete(const std::basic_string<T>& key) {
+    Node* node = root_;
+    for (std::size_t i = 0; i < key.size(); ++i) {
+      if (node->children_[key[i]] == nullptr) {
+        return;
+      }
+      node = node->children_[key[i]];
+    }
+    if (node->children_[key[key.size()]] == nullptr) {
+      return;
+    }
+    delete node->children_[key[key.size()]];
+    node->children_[key[key.size()]] = nullptr;
+  }
+  
+  bool Find(const std::basic_string<T>& key) {
+    Node* node = root_;
+    for (std::size_t i = 0; i <= key.size(); ++i) {
+      if (node->children_[key[i]] == nullptr) {
+        return false;
+      }
+      node = node->children_[key[i]];
     }
     return true;
   }
