096ad75027644ee621bd6c0c6181ae862d469f80
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-04-13 21:51:56 -0700

    Update dsu.h

--- dsu.h
+++ dsu.h
@@ -1,19 +1,23 @@
 #include <cstddef>
+#include <type_traits>
 #include <vector>
 
+template <class T, bool B = std::is_signed<T>::value>
+class Dsu;
+
 template <class T>
-class Dsu {
+class Dsu<T, false> {
  private:
   std::vector<T> parent_, size_;
  public:
-  Dsu(std::size_t size) : size_(size, 1), parent_([&]() {
+  Dsu(std::size_t size) : parent_([&]() {
     std::vector<T> parent;
     parent.reserve(size);
     for (std::size_t i = 0; i < size; ++i) {
       parent.emplace_back(i);
     }
     return parent;
-  }()) {}
+  }()), size_(size, 1), {}
 
   T Find(T vertex) {
     while (parent_[vertex] != vertex) {
@@ -25,16 +29,47 @@ class Dsu {
 
   bool Union(T left, T right) {
     left = Find(left);
-    right= Find(right);
+    right = Find(right);
     if (left == right) {
       return false;
     }
-    parent_[right] = left;
     size_[left] += size_[right];
+    parent_[right] = left;
     return true;
   }
 
   T Size(T vertex) {
     return size_[Find(vertex)];
+  }
+};
+
+template <class T>
+class Dsu<T, true> {
+ private:
+  std::vector<T> parent_or_size_;
+ public:
+  Dsu(std::size_t size) : parent_or_size_(size, -1) {}
+
+  T Find(T vertex) {
+    if (parent_or_size_[vertex] < 0) {
+      return vertex;
+    }
+    parent_or_size_[vertex] = Find(parent_or_size_[vertex]);
+    return parent_or_size_[vertex];
+  }
+
+  bool Union(T left, T right) {
+    left = Find(left);
+    right = Find(right);
+    if (left == right) {
+      return false;
+    }
+    parent_or_size_[left] += parent_or_size_[right];
+    parent_or_size_[right] = left;
+    return true;
+  }
+
+  T Size(T vertex) {
+    return -parent_or_size_[Find(vertex)];
   }
 };
