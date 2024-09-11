3eaf9016662b063b827f82e559784a29a5df75c6
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-09-11 15:24:54 -0700

    Update prim.h

--- prim.h
+++ prim.h
@@ -1,1 +1,27 @@
+#include <limits>
+#include <set>
+#include <vector>
 
+template<class U, class Neighbors, class T>
+U prim(const std::vector<Neighbors>& graph, const T& source) {
+  U result = 0;
+  std::vector<bool> selected(graph.size());
+  std::vector<U> dist(graph.size(), std::numeric_limits<U>::max());
+  dist[source] = 0;
+  std::set<std::pair<U, T>> edges;
+  edges.emplace(0, source);
+  do {
+    auto v = edges.begin()->second;
+    selected[v] = true;
+    result += edges.begin()->first;
+    edges.erase(edges.begin());
+    for (auto [u, w] : graph[v]) {
+      if (!selected[u] && w < dist[u]) {
+        edges.erase({dist[u], u});
+        dist[u] = w;
+        edges.emplace(w, u);
+      }
+    }
+  } while (!edges.empty());
+  return result;
+}
