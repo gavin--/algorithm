a3feca7c17bdb825c8951abf5a6a81c694769add
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-09-10 18:40:08 -0700

    Update dijkstra.h

--- dijkstra.h
+++ dijkstra.h
@@ -1,11 +1,34 @@
 #include <limits>
+#include <map>
 #include <set>
 #include <vector>
 
-#include "graph.h"
+template<class U, class Neighbors, class T>
+std::map<T, U> dijkstra(const std::map<T, Neighbors>& graph, const T& source) {
+  std::map<T, U> distance;
+  distance[source] = 0;
+  std::set<std::pair<U, T>> q;
+  q.emplace(0, source);
+  do {
+    auto [dist_v, v] = *q.begin();
+    q.erase(q.begin());
+    for (const auto& [u, w] : graph.at(v)) {
+      auto it = distance.find(u);
+      if (it == distance.end()) {
+        it = distance.insert({u, std::numeric_limits<U>::max()}).first;
+      }
+      if (it->second > dist_v + w) {
+        q.erase({it->second, u});
+        it->second = dist_v + w;
+        q.emplace(it->second, u);
+      }
+    }
+  } while (!q.empty());
+  return distance;
+}
 
 template<class U, class Neighbors, class T>
-std::vector<U> dijkstra(const Graph<Neighbors>& graph, const T& source) {
+std::vector<U> dijkstra(const std::vector<Neighbors>& graph, const T& source) {
   std::vector<U> distance(graph.size(), std::numeric_limits<U>::max());
   distance[source] = 0;
   std::set<std::pair<U, T>> q;
@@ -13,7 +36,7 @@ std::vector<U> dijkstra(const Graph<Neighbors>& graph, const T& source) {
   do {
     auto v = q.begin()->second;
     q.erase(q.begin());
-    for (auto [u, w] : graph[v]) {
+    for (const auto& [u, w] : graph[v]) {
       if (distance[u] > distance[v] + w) {
         q.erase({distance[u], u});
         distance[u] = distance[v] + w;
@@ -23,26 +46,3 @@ std::vector<U> dijkstra(const Graph<Neighbors>& graph, const T& source) {
   } while (!q.empty());
   return distance;
 }
-
-template<template<class, class> class Map, template <class...> class Container, template<class, class> class P, class T, class U>
-Map<T, U> dijkstra(const Map<T, Container<P<T, U>>>& graph, T source) {
-  Map<T, U> distance;
-  distance[source] = 0;
-  std::set<std::pair<U, T>> s;
-  s.emplace(0, source);
-  do {
-    auto vertex = s.begin()->second;
-    auto dist = distance[vertex];
-    s.erase(s.begin());
-    for (auto [next, weight] : graph.at(vertex)) {
-      auto& next_dist = distance[next];
-      if (next_dist > dist + weight) {
-        s.erase({next_dist, next});
-        next_dist = dist + weight;
-        s.emplace(next_dist, next);
-      }
-    }
-  } while (!s.empty());
-  return distance;
-}
-
