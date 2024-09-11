8f16feb3128dc95426c220bb1fa62cee3390ae31
gavin-- <gavinzhubbb4mp5@gmail.com>
2024-09-10 17:49:03 -0700

    Update dijkstra.h

--- dijkstra.h
+++ dijkstra.h
@@ -1,26 +1,26 @@
 #include <limits>
 #include <set>
-#include <span>
 #include <vector>
-#include <ranges>
 
-template<template <class...> class Container, template<class, class> class P, class T, class U>
-std::vector<U> dijkstra(std::span<const Container<P<T, U>>> graph, T source) {
+#include "graph.h"
+
+template<class U, class Neighbors, class T>
+std::vector<U> dijkstra(const Graph<Neighbors>& graph, const T& source) {
   std::vector<U> distance(graph.size(), std::numeric_limits<U>::max());
   distance[source] = 0;
-  std::set<std::pair<U, T>> s;
-  s.emplace(0, source);
+  std::set<std::pair<U, T>> q;
+  q.emplace(0, source);
   do {
-    auto vertex = s.begin()->second;
-    s.erase(s.begin());
-    for (auto [next, weight] : graph[vertex]) {
-      if (distance[next] > distance[vertex] + weight) {
-        s.erase({distance[next], next});
-        distance[next] = distance[vertex] + weight;
-        s.emplace(distance[next], next);
+    auto v = q.begin()->second;
+    q.erase(q.begin());
+    for (auto [u, w] : graph[v]) {
+      if (distance[u] > distance[v] + w) {
+        q.erase({distance[u], u});
+        distance[u] = distance[v] + w;
+        q.emplace(distance[u], u);
       }
     }
-  } while (!s.empty());
+  } while (!q.empty());
   return distance;
 }
 
@@ -46,7 +46,3 @@ Map<T, U> dijkstra(const Map<T, Container<P<T, U>>>& graph, T source) {
   return distance;
 }
 
-template<std::ranges::contiguous_range R, class T>
-auto dijkstra(R&& r, T source) {
-  return dijkstra(std::span<const std::ranges::range_value_t<R>>(r), source);
-}
