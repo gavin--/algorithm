7188e6b5eb0ca94979927e93fe39ef5ee88ec6aa
gavin-- <gavinzhubbb4mp5@gmail.com>
2025-02-18 14:06:54 -0800

    Update min_queue.h

--- min_queue.h
+++ min_queue.h
@@ -3,31 +3,27 @@
 #include <utility>
 #include <vector>
 
-template<class T, class Compare = std::less<T>>
+template <class T, class Compare = std::less<T>>
 class MinQueue {
  public:
-  auto empty() const {
-    return left_.empty() && right_.empty();
-  }
-  
-  auto size() const {
-    return left_.size() + right_.size();
-  }
-  
+  auto empty() const { return left_.empty() && right_.empty(); }
+
+  auto size() const { return left_.size() + right_.size(); }
+
   auto front() const {
     if (left_.empty()) {
       return right_.back().second;
     }
     return left_.front().second;
   }
-  
+
   auto back() const {
     if (right_.empty()) {
       return left_.back().second;
     }
     return right_.front().second;
   }
-  
+
   auto min() const {
     if (!left_.empty()) {
       if (!right_.empty()) {
@@ -37,11 +33,14 @@ class MinQueue {
     }
     return right_.back().first;
   }
-  
+
   void push(T value) {
-    left_.push_back({left_.empty() ? value : std::min(value, left_.back().second, Compare()), std::move(value)});
+    left_.push_back({left_.empty()
+                         ? value
+                         : std::min(value, left_.back().second, Compare()),
+                     std::move(value)});
   }
-  
+
   void pop() {
     if (right_.empty()) {
       left_.swap(right_);
@@ -49,16 +48,17 @@ class MinQueue {
     }
     right_.pop_back();
   }
-  
+
   void swap(MinQueue<T, Compare>& other) noexcept {
     left_.swap(other.left_);
     right_.swap(other.right_);
   }
+
  private:
   std::vector<std::pair<T, T>> left_, right_;
 };
 
-template<class T, class Compare>
+template <class T, class Compare>
 void swap(MinQueue<T, Compare>& left, MinQueue<T, Compare>& right) noexcept {
   left.swap(right);
 }
