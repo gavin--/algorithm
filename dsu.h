80aa74ac0465b4566c5486072ab339d333ff0c44
gavin-- <gavinzhubbb4mp5@gmail.com>
2025-02-21 17:09:26 -0800

    Update dsu.h

--- dsu.h
+++ dsu.h
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cstddef>
 #include <type_traits>
 #include <vector>
@@ -11,6 +12,7 @@ class Dsu<T, false> {
   Dsu(std::size_t size) : parent_or_size_(size, 1), is_parent_(size, false) {}
 
   T Find(T vertex) {
+    assert(0 <= vertex && vertex < parent_or_size_.size());
     while (is_parent_[vertex]) {
       if (is_parent_[parent_or_size_[vertex]]) {
         parent_or_size_[vertex] = parent_or_size_[parent_or_size_[vertex]];
@@ -21,6 +23,8 @@ class Dsu<T, false> {
   }
 
   bool Union(T left, T right) {
+    assert(0 <= left && left < parent_or_size_.size());
+    assert(0 <= right && right < parent_or_size_.size());
     left = Find(left);
     right = Find(right);
     if (left == right) {
@@ -32,7 +36,10 @@ class Dsu<T, false> {
     return true;
   }
 
-  T Size(T vertex) { return parent_or_size_[Find(vertex)]; }
+  T Size(T vertex) {
+    assert(0 <= vertex && vertex < parent_or_size_.size());
+    return parent_or_size_[Find(vertex)];
+  }
 
  private:
   std::vector<T> parent_or_size_;
@@ -45,6 +52,7 @@ class Dsu<T, true> {
   Dsu(std::size_t size) : parent_or_size_(size, -1) {}
 
   T Find(T vertex) {
+    assert(0 <= vertex && vertex < parent_or_size_.size());
     while (parent_or_size_[vertex] >= 0) {
       if (parent_or_size_[parent_or_size_[vertex]] >= 0) {
         parent_or_size_[vertex] = parent_or_size_[parent_or_size_[vertex]];
@@ -55,6 +63,8 @@ class Dsu<T, true> {
   }
 
   bool Union(T left, T right) {
+    assert(0 <= left && left < parent_or_size_.size());
+    assert(0 <= right && right < parent_or_size_.size());
     left = Find(left);
     right = Find(right);
     if (left == right) {
@@ -65,7 +75,10 @@ class Dsu<T, true> {
     return true;
   }
 
-  T Size(T vertex) { return -parent_or_size_[Find(vertex)]; }
+  T Size(T vertex) {
+    assert(0 <= vertex && vertex < parent_or_size_.size());
+    return -parent_or_size_[Find(vertex)];
+  }
 
  private:
   std::vector<T> parent_or_size_;
