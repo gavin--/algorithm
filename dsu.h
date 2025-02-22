064387efbf6fa2884be75dd33f0890fa61ba1866
gavin-- <gavinzhubbb4mp5@gmail.com>
2025-02-21 16:04:16 -0800

    Update dsu.h

--- dsu.h
+++ dsu.h
@@ -7,26 +7,20 @@ class Dsu;
 
 template <class T>
 class Dsu<T, false> {
- private:
-  std::vector<T> parent_or_size_;
-  std::vector<bool> is_parent_;
-
  public:
   Dsu(std::size_t size) : parent_or_size_(size, 1), is_parent_(size, false) {}
 
-  T Find(std::size_t vertex) {
+  T Find(T vertex) {
     while (is_parent_[vertex]) {
       if (is_parent_[parent_or_size_[vertex]]) {
         parent_or_size_[vertex] = parent_or_size_[parent_or_size_[vertex]];
-      } else {
-        parent_or_size_[vertex] = parent_or_size_[vertex];
       }
       vertex = parent_or_size_[vertex];
     }
     return vertex;
   }
 
-  bool Union(std::size_t left, std::size_t right) {
+  bool Union(T left, T right) {
     left = Find(left);
     right = Find(right);
     if (left == right) {
@@ -38,30 +32,29 @@ class Dsu<T, false> {
     return true;
   }
 
-  T Size(std::size_t vertex) { return parent_or_size_[Find(vertex)]; }
+  T Size(T vertex) { return parent_or_size_[Find(vertex)]; }
+
+ private:
+  std::vector<T> parent_or_size_;
+  std::vector<bool> is_parent_;
 };
 
 template <class T>
 class Dsu<T, true> {
- private:
-  std::vector<T> parent_or_size_;
-
  public:
   Dsu(std::size_t size) : parent_or_size_(size, -1) {}
 
-  T Find(std::size_t vertex) {
-    while (parent_or_size_[vertex] > 0) {
-      if (parent_or_size_[parent_or_size_[vertex]] > 0) {
+  T Find(T vertex) {
+    while (parent_or_size_[vertex] >= 0) {
+      if (parent_or_size_[parent_or_size_[vertex]] >= 0) {
         parent_or_size_[vertex] = parent_or_size_[parent_or_size_[vertex]];
-      } else {
-        parent_or_size_[vertex] = parent_or_size_[vertex];
       }
       vertex = parent_or_size_[vertex];
     }
     return vertex;
   }
 
-  bool Union(std::size_t left, std::size_t right) {
+  bool Union(T left, T right) {
     left = Find(left);
     right = Find(right);
     if (left == right) {
@@ -72,5 +65,8 @@ class Dsu<T, true> {
     return true;
   }
 
-  T Size(std::size_t vertex) { return -parent_or_size_[Find(vertex)]; }
+  T Size(T vertex) { return -parent_or_size_[Find(vertex)]; }
+
+ private:
+  std::vector<T> parent_or_size_;
 };
