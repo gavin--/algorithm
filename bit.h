60b43dac4ecfba98f510c813eb6ff0917a78ad66
gavin-- <gavinzhubbb4mp5@gmail.com>
2025-03-20 14:47:11 -0700

    Update bit.h

--- bit.h
+++ bit.h
@@ -17,7 +17,7 @@ class Bit {
   T Query(std::size_t index) const {
     assert(index <= tree_.size());
     T result = 0;
-    for (; index > 0; index -= Lowbit(index)) {
+    for (; index > 0; index -= index & -index) {
       result += tree_[index - 1];
     }
     return result;
@@ -25,19 +25,18 @@ class Bit {
 
   void Update(std::size_t index, T value) {
     assert(index < tree_.size());
-    for (++index; index <= tree_.size(); index += Lowbit(index)) {
-      tree_[index - 1] += value;
+    for (; index < tree_.size(); index = index | (index + 1)) {
+      tree_[index] += value;
     }
   }
 
  private:
   std::vector<T> tree_;
-  std::size_t Lowbit(std::size_t n) const { return n & -n; }
   explicit Bit(std::span<const T> tree) : Bit(tree.size()) {
-    for (std::size_t i = 1; i <= tree_.size(); ++i) {
-      tree_[i - 1] += tree[i - 1];
-      if (auto parent = i + Lowbit(i); parent <= tree_.size()) {
-        tree_[parent - 1] += tree_[i - 1];
+    for (std::size_t i = 0; i < tree_.size(); ++i) {
+      tree_[i] += tree[i];
+      if (auto parent = i | (i + 1); parent < tree_.size()) {
+        tree_[parent] += tree_[i];
       }
     }
   }
