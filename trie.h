7a2b8c7af1672000fa592a0d4bf90e142685b008
gavin-- <gavinzhubbb4mp5@gmail.com>
2025-04-07 14:18:05 -0700

    Update trie.h

--- trie.h
+++ trie.h
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <map>
+#include <queue>
 #include <string_view>
 #include <vector>
 
@@ -9,7 +10,8 @@ class PrefixTrie {
   PrefixTrie() : trie_(1) {}
 
   void Insert(std::basic_string_view<T> key) {
-    for (std::size_t i = 0, t = 0; i <= key.size(); ++i) {
+    std::size_t t = 0;
+    for (std::size_t i = 0; i < key.size(); ++i) {
       auto& child_node = trie_[t][key[i]];
       if (child_node == 0) {
         child_node = trie_.size();
@@ -17,15 +19,23 @@ class PrefixTrie {
       }
       t = child_node;
     }
+    if (auto& child_node = trie_[t][T()]; child_node == 0) {
+      child_node = trie_.size();
+      trie_.push_back({});
+    }
   }
 
   bool Find(std::basic_string_view<T> key) const {
-    for (std::size_t i = 0, t = 0; i <= key.size(); ++i) {
+    std::size_t t = 0;
+    for (std::size_t i = 0; i < key.size(); ++i) {
       auto it = trie_[t].find(key[i]);
       if (it == trie_[t].end()) {
         return false;
       }
       t = it->second;
+    }
+    if (auto it = trie_[t].find(T()); it == trie_[t].end()) {
+      return false;
     }
     return true;
   }
@@ -48,14 +58,30 @@ class PrefixTrie {
 template <class T>
 class Trie {
  public:
+  ~Trie() {
+    std::queue<Node*> q;
+    q.push(root_);
+    do {
+      auto* const node = q.front();
+      for (auto& [_, v] : node->children) {
+        q.push(v);
+      }
+      delete node;
+      q.pop();
+    } while (!q.empty());
+  }
+
   void Insert(std::basic_string_view<T> key) {
     Node* node = root_;
-    for (std::size_t i = 0; i <= key.size(); ++i) {
+    for (std::size_t i = 0; i < key.size(); ++i) {
       auto& child_node = node->children[key[i]];
       if (child_node == nullptr) {
         child_node = new Node();
       }
       node = child_node;
+    }
+    if (auto& child_node = node->children[T()]; child_node == nullptr) {
+      child_node = new Node();
     }
   }
 
@@ -79,12 +105,15 @@ class Trie {
 
   bool Find(std::basic_string_view<T> key) const {
     Node* node = root_;
-    for (std::size_t i = 0; i <= key.size(); ++i) {
+    for (std::size_t i = 0; i < key.size(); ++i) {
       if (auto it = node->children.find(key[i]); it == node->children.end()) {
         return false;
       } else {
         node = it->second;
       }
+    }
+    if (auto it = node->children.find(T()); it == node->children.end()) {
+      return false;
     }
     return true;
   }
